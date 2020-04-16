efb27072b6354a1aec776590fb30e27968dee44e
Shakir Davis <Shakir.davis@Gmail.com>
2020-04-16 16:24:08 -0400

    Updates

---
+++ precache-manifest.6be048b0ff06d3fd034fc14abc03360a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc234f84e40bf50c398961b5fca50fb0",
+    "url": "/covid-19_tracker/index.html"
+  },
+  {
+    "revision": "5fc60916ff7ead11cabf",
+    "url": "/covid-19_tracker/static/css/main.5b893e30.chunk.css"
+  },
+  {
+    "revision": "c2667c794591cbbc636f",
+    "url": "/covid-19_tracker/static/js/2.d64780be.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19_tracker/static/js/2.d64780be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc60916ff7ead11cabf",
+    "url": "/covid-19_tracker/static/js/main.b86c34bc.chunk.js"
+  },
+  {
+    "revision": "a3f66d27ca16951030cd",
+    "url": "/covid-19_tracker/static/js/runtime-main.a8d75d8d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_tracker/static/media/covid-19.d7265326.png"
+  }
+]);
